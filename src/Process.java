a235095b19711e97e51f4db4b6791df35d770611
yun318 <dbsrud318@naver.com>
2020-11-18 22:28:38 +0900

    ClassProcess

--- src/Process.java
+++ src/Process.java
@@ -1,4 +1,70 @@
 
 public class Process {
+	private String pid;
+	private int arriveTime, returnTime, waitingTime, responseTime, burstTime, turnAroundTime, priority;
+
+	public String getPid() {
+		return pid;
+	}
+
+	public void setPid(String pid) {
+		this.pid = pid;
+	}
+
+	public int getArriveTime() {
+		return arriveTime;
+	}
+
+	public void setArriveTime(int arriveTime) {
+		this.arriveTime = arriveTime;
+	}
+
+	public int getReturnTime() {
+		return returnTime;
+	}
+
+	public void setReturnTime(int returnTime) {
+		this.returnTime = returnTime;
+	}
+
+	public int getWaitingTime() {
+		return waitingTime;
+	}
+
+	public void setWaitingTime(int waitingTime) {
+		this.waitingTime = waitingTime;
+	}
+
+	public int getResponseTime() {
+		return responseTime;
+	}
+
+	public void setResponseTime(int responseTime) {
+		this.responseTime = responseTime;
+	}
+
+	public int getBurstTime() {
+		return burstTime;
+	}
+
+	public void setBurstTime(int burstTime) {
+		this.burstTime = burstTime;
+	}
+
+	public int getTurnAroundTime() {
+		return turnAroundTime;
+	}
+
+	public void setTurnAroundTime(int turnAroundTime) {
+		this.turnAroundTime = turnAroundTime;
+	}
+
+	public int getPriority() {
+		return priority;
+	}
+
+	public void setPriority(int priority) {
+		this.priority = priority;
+	}
 
 }
--- src/SchedulingManager.java
+++ src/SchedulingManager.java
@@ -1,4 +1,58 @@
+import java.util.ArrayList;
+import java.util.List;
 
-public class SchedulingManager {
-
+public abstract class SchedulingManager {
+	private List<Process> processes;	// 프로세스들의 list
+	// private List<Event> timeflow; 		// 간트차트 그리기용 list(?)
+	private int timeQuantum;			// 시간할당량(타임슬라이스)
+	
+	/* Scheduling Manager 클래스의 생성자 */
+	public SchedulingManager() {
+		processes = new ArrayList();
+		// timeflow = new ArrayList();
+		timeQuantum = 1; // 시간할당량(타임슬라이스)은 초기값 1로 지정
+	}
+	
+	/* 시간할당량(타임슬라이스) setter */
+	public void setTimeQuantum(int timeQuantum) {
+		this.timeQuantum = timeQuantum;
+	}
+	
+	/* 시간할당량(타임슬라이스) getter */
+	public int getTimeQuantum() {
+		return this.timeQuantum;
+	}
+	
+	/* AWT(평균 대기 시간) getter */
+	public double getAvgWaitingTime() {
+		double avg = 0.0;
+		
+		for (Process process : processes) {
+			avg += process.getWaitingTime();
+		}
+		
+		return avg / processes.size(); 
+	}
+	
+	/* ART(평균 응답 시간) getter */
+	public double getAvgResponseTime() {
+        double avg = 0.0;
+        
+        for (Process process : processes) {
+            avg += process.getResponseTime();
+        }
+        
+        return avg / processes.size();
+    }
+	
+	/* ATT(평균 반환 시간) getter */
+	public double getAvgTurnAroundTime() {
+        double avg = 0.0;
+        
+        for (Process process : processes) {
+            avg += process.getTurnAroundTime();
+        }
+        
+        return avg / processes.size();
+    }
 }
