1dc265a0f6e15f52806ab4db30a6b559a877758e
ningpop <craft1933@naver.com>
2020-11-27 06:26:23 +0900

    fix Process class

--- src/Process.java
+++ src/Process.java
@@ -14,23 +14,19 @@ public class Process {
 	private Color color;		// 색상 변수
 	
 	
-	private Process(String pid, int arriveTime, int burstTime, int priority, int waitingTime, int turnAroundTime, Color color) {
+	private Process(String pid, int arriveTime, int burstTime, int priority, int waitingTime, int turnAroundTime) {
         this.pid = pid;
         this.arriveTime = arriveTime;
         this.burstTime = burstTime;
         this.priority = priority;
         this.waitingTime = waitingTime;
         this.turnAroundTime = turnAroundTime;
-        this.color = color;
+        this.color = new Color((int)(Math.random()*255),(int)(Math.random()*255),(int)(Math.random()*255));
     }
 
 	public Process(String pid, int arriveTime, int burstTime,
-    		int priority, Color color) {
-        this(pid, arriveTime, burstTime, priority, 0, 0, color);
-    }
-    
-    public Process(String pid, int arriveTime, int burstTime, Color color) {
-        this(pid, arriveTime, burstTime, 0, 0, 0, color);
+    		int priority) {
+        this(pid, arriveTime, burstTime, priority, 0, 0);
     }
 	
 	// 프로세스 이름 반환
